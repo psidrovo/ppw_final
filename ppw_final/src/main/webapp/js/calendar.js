d53d02ac3eade5d76b0f4d5bb6fa5b0cb313d2f8
pidrovo <paulsidrovob@gmail.com>
2022-02-09 22:10:34 -0500

    Correcciones minimas Se modifico el calendario y se agrego la vista de los pacientes

--- ppw_final/src/main/webapp/js/calendar.js
+++ ppw_final/src/main/webapp/js/calendar.js
@@ -28,35 +28,37 @@ $(document).ready(function(t, e, i) {
             week: "week",
             day: "day"
         },
-        events: [ {
-            title: "Long Event",
-            start: new Date(r, n, i - 5),
-            end: new Date(r, n, i - 2),
-            className: "bg-yellow"
-        }, {
-            title: "Meeting",
-            start: new Date(r, n, i, 10, 30),
-            allDay: !1,
-            className: "bg-red"
-        }, {
-            title: "Lunch",
-            start: new Date(r, n, i, 12, 0),
-            end: new Date(r, n, i, 14, 0),
-            allDay: !1,
-            className: "bg-navy"
-        }, {
-            title: "Birthday Party",
-            start: new Date(r, n, i + 1, 19, 0),
-            end: new Date(r, n, i + 1, 22, 30),
-            allDay: !1,
-            className: "bg-green"
-        }, {
-            title: "Click for Google",
-            start: new Date(r, n, 28),
-            end: new Date(r, n, 29),
-            url: "http://google.com/",
-            className: "bg-lime"
-        }],
+        /*
+                events: [{
+                    title: "Long Event",
+                    start: new Date(r, n, i - 5),
+                    end: new Date(r, n, i - 2),
+                    className: "bg-yellow"
+                }, {
+                    title: "Meeting",
+                    start: new Date(r, n, i, 10, 30),
+                    allDay: !1,
+                    className: "bg-red"
+                }, {
+                    title: "Lunch",
+                    start: new Date(r, n, i, 12, 0),
+                    end: new Date(r, n, i, 14, 0),
+                    allDay: !1,
+                    className: "bg-navy"
+                }, {
+                    title: "Birthday Party",
+                    start: new Date(r, n, i + 1, 19, 0),
+                    end: new Date(r, n, i + 1, 22, 30),
+                    allDay: !1,
+                    className: "bg-green"
+                }, {
+                    title: "Click for Google",
+                    start: new Date(r, n, 28),
+                    end: new Date(r, n, 29),
+                    url: "http://facebook.com/",
+                    className: "bg-lime"
+                }],
+        */
         editable: !0,
         selectable: !0,
         droppable: !0,
